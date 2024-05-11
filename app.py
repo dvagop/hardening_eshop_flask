3d45073c4dee6cddd4967973abac6b2c995ec03f
radaplanakos <142523340+radaplanakos@users.noreply.github.com>
2024-05-11 06:28:02 +0000

    add database dynamic

--- app.py
+++ app.py
@@ -1,43 +1,17 @@
 from flask import Flask, render_template, jsonify
+from database import load_jobs_from_db
 
 app = Flask(__name__)
 
-JOBS = [
-    {
-        'id': 1,
-        'title': 'Brotherhood of Steel',
-        'location': 'The Hidden Valley',
-        'salary': 'Euro. 1,000'
-    },
-
-    {
-        'id': 2,
-        'title': 'Vault Dweller',
-        'location': 'The Vault',
-        'salary': 'Euro. 10,000'
-    },
-
-    {   'id': 3,
-        'title': 'The Ghoul',
-        'location': 'The Wasteland',
-        'salary': 'Euro. 100'
-        
-    },
-
-    {    'id': 4,
-         'title': 'The Rebel',
-         'location': 'New California Republic',
-         
-    }
-]
-
 @app.route("/")
 def index():
-    return render_template('home.html', jobs=JOBS, owner='Fallout')
+    jobs=load_jobs_from_db()
+    return render_template('home.html', jobs=jobs, owner='Fallout')
 
 @app.route("/api/jobs")
 def list_jobs():
-    return jsonify(JOBS)
+    jobs=load_jobs_from_db()
+    return jsonify(jobs)
 
 if __name__=="__main__":
     app.run(host='0.0.0.0', debug=True)
